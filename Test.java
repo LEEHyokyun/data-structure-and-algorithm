31268e4e56088728ddc8597c26ac16f4763f4767
leehyokyun <gyrbs22@gmail.com>
2025-10-17 17:40:15 +0900

    test running environment init

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+        System.out.println("*****************TEST**************");
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
