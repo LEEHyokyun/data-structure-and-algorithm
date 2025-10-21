c630218b2dd58058380b16af3ecaa630e196feb9
leehyokyun <gyrbs22@gmail.com>
2025-10-21 11:43:03 +0900

    Stack

--- basic/stack/Stack.java
+++ basic/stack/Stack.java
@@ -1,4 +1,36 @@
 package basic.stack;
 
+import basic.linkedList.LinkedList;
+import basic.linkedList.Node;
+
 public class Stack {
+    private LinkedList linkedList = new LinkedList(new Node("1", null));
+
+    public Stack(LinkedList linkedList){
+        this.linkedList = linkedList;
+    }
+
+    //1
+    public void push(String data){
+        this.linkedList.insertAt(0L, "1");
+    }
+
+    //2
+    public void pop(){
+        try {
+            this.linkedList.deleteAt(0L);
+        } catch(Exception e){
+            throw new RuntimeException("<UNK>");
+        }
+    }
+
+    //3
+    public void peek(){
+        this.linkedList.getNodeAt(0L);
+    }
+
+    //4
+    public boolean isEmpty(){
+        return this.linkedList.getCount() == 0;
+    }
 }
