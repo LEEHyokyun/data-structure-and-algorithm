205556f0b6a887cf05fef4b2d02a2e8aaba56651
leehyokyun <gyrbs22@gmail.com>
2025-10-21 12:51:20 +0900

    queue

---
+++ basic/queue/Queue.java
@@ -0,0 +1,36 @@
+package basic.queue;
+
+import basic.doublyLinkedList.DoublyLinkedList;
+import basic.doublyLinkedList.Node;
+
+public class Queue {
+    private DoublyLinkedList doublyLinkedList = new DoublyLinkedList(new Node("1", null, null);
+
+    public Queue(DoublyLinkedList doublyLinkedList){
+        this.doublyLinkedList = doublyLinkedList;
+    }
+
+    //1
+    public void enqueue(){
+        this.doublyLinkedList.insertAt(0L, "enqueue");
+    }
+
+    //2
+    public void dequeue(){
+        try {
+            this.doublyLinkedList.deleteLast();
+        } catch(Exception e){
+            throw new RuntimeException("<UNK>");
+        }
+    }
+
+    //3
+    public void front(){
+        this.doublyLinkedList.getTail();
+    }
+
+    //4
+    public boolean isEmpty(){
+        return this.doublyLinkedList.getCount() == 0;
+    }
+}
