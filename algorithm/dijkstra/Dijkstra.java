237f81b70ac41c6bbf5332f1945fe8801ddab4a1
leehyokyun <gyrbs22@gmail.com>
2025-11-04 13:31:56 +0900

    add comment

--- algorithm/dijkstra/Dijkstra.java
+++ algorithm/dijkstra/Dijkstra.java
@@ -39,10 +39,18 @@ public class Dijkstra {
         dijkstraTable.put(startCityVertex.getName(), 0D);
 
         //dijkstra
+        /*
+        * 최초 반복문에서 이미 최초 시작점의 정점의 인접노드들로 테이블을 갱신해주었으므로, 다음 순회도 그 인접노드에 대해 반복하고,
+        * 인접노드에 대해 방문하지 않아도 이미 그 노드에 방문하기 전까지의 경로가 모두 누적갱신된 상태.
+        * */
         while(!unvisitedCities.isEmpty()){
             //최초 상태를 정의하며, 이 이후에 그 상태가 항상 누적된다(*객체비교).
             WeightedGraphCityVertex currentCityVertex = null;
 
+            /*
+            * 최초 상태는 null
+            * for 반복순회를 하면서 미방문 도시들 중, 가장 가중치가 작은 정점을 선택하여 다익스트라 테이블을 갱신한다.
+            * */
             for(WeightedGraphCityVertex cityVertex: unvisitedCities.values()){
                 if(currentCityVertex == null || dijkstraTable.get(cityVertex.getName()) < dijkstraTable.get(currentCityVertex.getName())){
                     currentCityVertex = cityVertex;
