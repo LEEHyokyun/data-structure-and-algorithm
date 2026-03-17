6c64b3b9f3b197fcd30003cdf440fb7b2f1b9b21
leehyokyun <gyrbs22@gmail.com>
2026-03-17 11:45:36 +0900

    day5

--- week19/UserTheStairsBFS.java
+++ week19/UserTheStairsBFS.java
@@ -3,6 +3,8 @@ package week19;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class UserTheStairsBFS {
